5c4e008c5be33e620cdbdb37caaf4b82f19dd661
Aqrun <aqrun@sina.com>
2019-10-03 19:14:52 +0800

    add progress bar

--- webapps/frontend/config/webpack.config.js
+++ webapps/frontend/config/webpack.config.js
@@ -26,6 +26,8 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 const eslint = require('eslint');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const chalk = require('chalk');
 
 const postcssNormalize = require('postcss-normalize');
 
@@ -665,6 +667,11 @@ module.exports = function(webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
         }),
+
+        new ProgressBarPlugin({
+          format: '  build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
+          clear: false
+        })
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
